4a3cade31b094c46f18297707494e8c8aac257e4
EC2 Default User <ec2-user@ip-172-31-30-251.us-west-1.compute.internal>
2023-07-26 12:26:43 +0000

    Done App2 Chapter26

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = current_user if user_signed_in?
+  end
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
@@ -15,6 +20,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
   end
 end
